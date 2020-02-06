b9f27f1ca3e76becd13d25813bd54b44f183dd80
APortoghese <aportoghese20@moundsparkacademy.org>
2020-02-05 18:57:39 -0600

    AH

--- Falcon 500 Drive Base/src/main/java/frc/robot/Robot.java
+++ Falcon 500 Drive Base/src/main/java/frc/robot/Robot.java
@@ -7,6 +7,8 @@
 
 package frc.robot;
 
+import java.util.StringJoiner;
+
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.InvertType;
@@ -15,15 +17,23 @@ import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
 import com.ctre.phoenix.sensors.SensorInitializationStrategy;
 
+import edu.wpi.first.hal.HAL;
+import edu.wpi.first.hal.FRCNetComm.tInstances;
+import edu.wpi.first.hal.FRCNetComm.tResourceType;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Sendable;
 import edu.wpi.first.wpilibj.SpeedController;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.drive.RobotDriveBase;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SendableBuilder;
+import edu.wpi.first.wpilibj.smartdashboard.SendableRegistry;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpiutil.math.MathUtil;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -173,5 +183,140 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void testPeriodic() {
+  }
+}
+
+class AnnyDDrive extends RobotDriveBase implements Sendable, AutoCloseable {
+  public static final double kDefaultQuickStopThreshold = 0.2;
+  public static final double kDefaultQuickStopAlpha = 0.1;
+
+  private static int instances;
+
+  private final TalonFX m_FR;
+  private final TalonFX m_BR;
+  private final TalonFX m_FL;
+  private final TalonFX m_BL;
+
+  private double m_quickStopThreshold = kDefaultQuickStopThreshold;
+  private double m_quickStopAlpha = kDefaultQuickStopAlpha;
+  private double m_quickStopAccumulator;
+  private double m_rightSideInvertMultiplier = -1.0;
+  private boolean m_reported;
+
+  public AnnyDDrive(TalonFX FR, TalonFX BR, TalonFX FL, TalonFX BL) {
+      verify(FR, BR, FL, BL);
+      m_FR = FR;
+      m_BR = BR;
+      m_FL = FL;
+      m_BL = BL;
+      SendableRegistry.addChild(this, FR);
+      SendableRegistry.addChild(this, BR);
+      SendableRegistry.addChild(this, FL);
+      SendableRegistry.addChild(this, BL);
+      m_FR.follow(BR);
+      m_FL.follow(BL);
+      instances++;
+      SendableRegistry.addLW(this, "AnnyDDrive", instances);
+  }
+
+
+
+  @SuppressWarnings("PMD.AvoidThrowingNullPointerException")
+  private void verify(TalonFX FR, TalonFX BR, TalonFX FL, TalonFX BL) {
+      if (FR != null && BR != null && FL != null && BL != null)
+          return; 
+      StringJoiner joiner = new StringJoiner(", ");
+      if (FR == null) {
+          joiner.add("FR");
+      }
+      if (BR == null) {
+          joiner.add("BR");
+      }
+      if (FL == null) {
+          joiner.add("FL");
+      }
+      if (BL == null) {
+          joiner.add("BL");
+      }
+      throw new NullPointerException(joiner.toString());  
+  }
+  @Override
+  public void close() throws Exception {
+      SendableRegistry.remove(this);
+  }
+  @SuppressWarnings("ParameterName")
+  public void arcadeDrive(double xSpeed, double zRotation) {
+      arcadeDrive(xSpeed, zRotation, true);
+  }
+  @SuppressWarnings("ParameterName")
+  public void arcadeDrive(double xSpeed, double zRotation, boolean squareInputs) {
+      if (!m_reported) {
+      HAL.report(tResourceType.kResourceType_RobotDrive,
+                  tInstances.kRobotDrive2_DifferentialArcade, 2);
+      m_reported = true;
+      }
+
+      xSpeed = MathUtil.clamp(xSpeed, -1.0, 1.0);
+      xSpeed = applyDeadband(xSpeed, m_deadband);
+
+      zRotation = MathUtil.clamp(zRotation, -1.0, 1.0);
+      zRotation = applyDeadband(zRotation, m_deadband);
+
+      // Square the inputs (while preserving the sign) to increase fine control
+      // while permitting full power.
+      if (squareInputs) {
+      xSpeed = Math.copySign(xSpeed * xSpeed, xSpeed);
+      zRotation = Math.copySign(zRotation * zRotation, zRotation);
+      }
+
+      double leftMotorOutput;
+      double rightMotorOutput;
+
+      double maxInput = Math.copySign(Math.max(Math.abs(xSpeed), Math.abs(zRotation)), xSpeed);
+
+      if (xSpeed >= 0.0) {
+      // First quadrant, else second quadrant
+      if (zRotation >= 0.0) {
+          leftMotorOutput = maxInput;
+          rightMotorOutput = xSpeed - zRotation;
+      } else {
+          leftMotorOutput = xSpeed + zRotation;
+          rightMotorOutput = maxInput;
+      }
+      } else {
+      // Third quadrant, else fourth quadrant
+      if (zRotation >= 0.0) {
+          leftMotorOutput = xSpeed + zRotation;
+          rightMotorOutput = maxInput;
+      } else {
+          leftMotorOutput = maxInput;
+          rightMotorOutput = xSpeed - zRotation;
+      }
+      }
+
+      m_BL.set(ControlMode.PercentOutput, MathUtil.clamp(leftMotorOutput, -1.0, 1.0) * m_maxOutput);
+      double maxOutput = m_maxOutput * m_rightSideInvertMultiplier;
+      m_BL.set(ControlMode.PercentOutput, MathUtil.clamp(rightMotorOutput, -1.0, 1.0) * maxOutput);
+
+      feed();
+  }
+  @Override
+  public void initSendable(SendableBuilder builder) {
+      // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void stopMotor() {
+      m_BL.set(ControlMode.PercentOutput, 0);
+      m_BR.set(ControlMode.PercentOutput, 0);
+      m_FL.set(ControlMode.PercentOutput, 0);
+      m_FR.set(ControlMode.PercentOutput, 0);
+      feed();
+  }
+
+  @Override
+  public String getDescription() {
+      return "Anny's DifferentialDrive to work with TalonFX";
   }
 }
