ab7baa4eb23a2343007425665b19b88b4d418bd5
Michael Wilke <mwilke@enarchtech.com>
2021-02-16 21:56:45 -0600

    Working branch auto example

--- 2021/Auto/src/main/java/frc/robot/Robot.java
+++ 2021/Auto/src/main/java/frc/robot/Robot.java
@@ -5,7 +5,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
  * each mode, as described in the TimedRobot documentation. If you change the name of this class or
@@ -13,24 +13,36 @@ import edu.wpi.first.wpilibj.TimedRobot;
  * project.
  */
 public class Robot extends TimedRobot {
+  RobotContainer container;
+
+  
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
    */
   @Override
-  public void robotInit() {}
+  public void robotInit() {
+    container = new RobotContainer();
+  }
 
   @Override
   public void robotPeriodic() {}
 
-  @Override
-  public void autonomousInit() {}
+  @Override //Runs once when you initialize Auto
+  public void autonomousInit() {
+    container.reset();
+    container.getAutonomousCommand().schedule();
+  }
+
+  @Override //Runs Periodicaly during Auto (Every 20 ms)
+  public void autonomousPeriodic() {
+    CommandScheduler.getInstance().run();
+  }
 
   @Override
-  public void autonomousPeriodic() {}
-
-  @Override
-  public void teleopInit() {}
+  public void teleopInit() {
+    container.reset();
+  }
 
   @Override
   public void teleopPeriodic() {}
